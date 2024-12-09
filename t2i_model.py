0c128c9ef8a7ed81c27911aac88108b5a5a79205
NMesaC <NMesaC1019@gmail.com>
2024-12-09 05:17:58 +0000

    Implemented embedding model

--- t2i_model.py
+++ t2i_model.py
@@ -46,7 +46,6 @@ def load_vqgan(config, ckpt_path=None, is_gumbel=False):
 """
 Model 1: Encoder Input
 """
-
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class T2IEncoderInput(nn.Module):
@@ -64,12 +63,13 @@ class T2IEncoderInput(nn.Module):
         # CLIP Text Model
         self.clip_txt = CLIPTextModelWithProjection.from_pretrained("openai/clip-vit-base-patch32").to(device)
         self.clip_tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-        # Initialize Linear Layer Stack
-        self.grounding_stack = nn.ModuleList([
-                    nn.Linear(d_proj, res_h_dim ** 2),
-                    nn.Linear(d_proj, res_h_dim ** 2),
-                    nn.Linear(d_proj, res_h_dim ** 2)
-                ])
+        # Initialize Linear Layer
+        self.grounding_layer = nn.Linear(d_proj, self.num_channels * (self.res_h_dim ** 2))
+        # self.grounding_stack = nn.ModuleList([
+        #             nn.Linear(d_proj, res_h_dim ** 2),
+        #             nn.Linear(d_proj, res_h_dim ** 2),
+        #             nn.Linear(d_proj, res_h_dim ** 2)
+        #         ])
         # Initialize VQGAN Config
         config = load_config("logs/vqgan_gumbel_f8/configs/model.yaml", display=False)
         self.vqgan = load_vqgan(config, ckpt_path=ckpt_path, is_gumbel=is_gumbel).to(device)
@@ -80,6 +80,7 @@ class T2IEncoderInput(nn.Module):
         for param in self.vqgan.parameters():
             param.requires_grad = False
 
+
     def encode_text(self, x):
         # Tokenize text
         inputs = self.clip_tokenizer(x, padding = "max_length",
@@ -89,7 +90,8 @@ class T2IEncoderInput(nn.Module):
         # Get the batch size
         b, _ = txt_features.shape
         # Project it into the encoder input dimension
-        text_embd = torch.stack([layer(txt_features) for layer in self.grounding_stack],dim = 1)
+        # text_embd = torch.stack([layer(txt_features) for layer in self.grounding_stack],dim = 1)
+        text_embd = self.grounding_layer(txt_features)
         # Return a [b, 3, res_h_dim, res_h_dim] view of our grounded text features
         return text_embd.view((b,self.num_channels,self.res_h_dim,self.res_h_dim))
 
@@ -100,5 +102,55 @@ class T2IEncoderInput(nn.Module):
         x = preprocess_vqgan(x)
         # Encode text with VQGAN Encoder
         z, _, _ = self.vqgan.encode(x)
+        # Return a reconstructed version of the encoded text embedding
+        return self.vqgan.decode(z)
+
+"""
+Model 2: Embedding Input
+"""
+class T2IEmbeddingInput(nn.Module):
+    def __init__(self, d_proj : int,
+                       res_h_dim : int,
+                       config_path : str,
+                       ckpt_path : str,
+                       is_gumbel : bool,
+    ):
+        super(T2IEmbeddingInput, self).__init__()
+        # Store variables
+        self.d_proj = d_proj
+        self.res_h_dim = res_h_dim
+        self.num_channels = 256
+        # CLIP Text Model
+        self.clip_txt = CLIPTextModelWithProjection.from_pretrained("openai/clip-vit-base-patch32").to(device)
+        self.clip_tokenizer = AutoTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+        # Initialize grounding layer
+        self.grounding_layer = nn.Linear(d_proj, self.num_channels * (self.res_h_dim ** 2))
+        # Initialize VQGAN Config
+        config = load_config("logs/vqgan_gumbel_f8/configs/model.yaml", display=False)
+        self.vqgan = load_vqgan(config, ckpt_path=ckpt_path, is_gumbel=is_gumbel).to(device)
+        # Freeze CLIP Modules
+        for param in self.clip_txt.parameters():
+            param.requires_grad = False
+        # Freeze VQGAN modules
+        for param in self.vqgan.parameters():
+            param.requires_grad = False
+
+
+    def encode_text(self, x):
+        # Tokenize text
+        inputs = self.clip_tokenizer(x, padding = "max_length",
+            max_length = 77, truncation = True, return_tensors = "pt").to(device)
+        # Get text features
+        txt_features = self.clip_txt(**inputs).text_embeds
+        # Get the batch size
+        b, _ = txt_features.shape
+        # Project it into the encoder input dimension
+        text_embd = self.grounding_layer(txt_features)
+        # Return a [b, 256, 32, 32] view of our grounded text features
+        return text_embd.view((b,self.num_channels,self.res_h_dim,self.res_h_dim))
+
+    def forward(self, x):
+        # Encode text and project into embedding space
+        z = self.encode_text(x)
         # Return a reconstructed version of the encoded text embedding
         return self.vqgan.decode(z)
--- t2i_train.py
+++ t2i_train.py
@@ -32,47 +32,64 @@ def normalize_output(x):
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Initialize Variables
-batch_size  = 1
+batch_size  = 256
 d_proj      = 512
-d_hidden    = 512
+d_hidden    = 32
 
 # Initialize model
 config_path = "logs/vqgan_gumbel_f8/configs/model.yaml"
 chkpt_path = "logs/vqgan_gumbel_f8/checkpoints/last.ckpt"
 model = T2IEncoderInput(d_proj, d_hidden, config_path, chkpt_path, True)
+model_path = "embedding_input.pth"
+# model.load_state_dict(torch.load(model_path))
 model.to(device)
 
 
 # Initialize dataloader
 transform = T.Compose([
     T.PILToTensor(), # Convert all PIL objects to tensors
-    T.Resize((512, 512)),  # Resize all images to 640x640
+    T.Resize((d_hidden, d_hidden)),  # Resize all images to d_h x d_h
+    T.ConvertImageDtype(torch.float),
 ])
 
-dataset = datasets.CocoCaptions(root = 'coco_data/content/val2017/val2017',
-                                annFile = 'coco_data/content/annotations/annotations/captions_val2017.json',
+# Define a custom collate_fn
+def custom_collate_fn(batch):
+    images, captions = zip(*batch)
+
+    # Find the max height and width in the batch
+    max_height = max(img.shape[1] for img in images)
+    max_width  = max(img.shape[2] for img in images)
+
+    # Pad images to the max dimensions
+    padded_images = [
+        F.pad(img, (0, max_width - img.shape[2], 0, max_height - img.shape[1]))
+        for img in images
+    ]
+
+    # Stack images and return with captions
+    return torch.stack(padded_images), captions
+
+dataset = datasets.CocoCaptions(root = 'coco_data/content/train2017/train2017',
+                                annFile = 'coco_data/content/annotations/annotations/captions_train2017.json',
                                 transform=transform)
 
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn = custom_collate_fn)
 
 # Setup optimizer
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.AdamW(model.parameters(), lr=1e-4)
 
 # Setup MSE Loss
 loss_fn = nn.MSELoss()
 
 # Training loop
-num_epochs = 1
+num_epochs = 2
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
     num_batches = 0
     for (img, txt) in tqdm(dataloader):
-        # Take the first set of captions
-        txt = txt[0]
-
-        # Convert captions into a list
-        txt = [t for t in txt]
+        # Convert caption tuple options into a list
+        txt = [t[0] for t in txt]
 
         # Forward pass
         optimizer.zero_grad()
@@ -80,9 +97,6 @@ for epoch in range(num_epochs):
         # Pass it through our model and get the logits
         txt_to_img = model(x = txt)
         txt_to_img = torch.stack([normalize_output(t) for t in txt_to_img], dim = 0)
-
-        # Convert both gt to float and scale
-        img = img.to(torch.float32) / 255.0
 
         # Compute loss
         loss   = loss_fn(txt_to_img, img.to(device))
@@ -97,6 +111,7 @@ for epoch in range(num_epochs):
         # Clear cache if necessary
         # print(f"GPU Memory in Use = {torch.cuda.memory_allocated() / 1024**3}")
         torch.cuda.empty_cache()
+        del loss
 
         # Print current loss for this batch
         # print(f"Loss for this batch: {loss.item()}")
@@ -106,7 +121,7 @@ for epoch in range(num_epochs):
 
         # Make a checkpoint every 100 batches
         if (num_batches % 100 == 0):
-            torch.save(model.state_dict(), "encoder_input.pth")
+            torch.save(model.state_dict(), model_path)
 
         # Print the average loss every 10 batches
         if (num_batches % 10 == 0):
@@ -115,4 +130,4 @@ for epoch in range(num_epochs):
     print(f"Total Loss: {total_loss/len(dataloader)}")
 
 # Save the trained model
-torch.save(model.state_dict(), "encoder_input.pth")
+torch.save(model.state_dict(), model_path)
